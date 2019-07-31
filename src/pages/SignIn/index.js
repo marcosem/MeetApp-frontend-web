94714292bcba7eb40cb9e5665257dc42ff9c4983
Marcos Mathias <marcos-github@memathias.com>
2019-07-31 18:24:07 -0300

    Authentication Stylization

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/logo.png';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+
+      <Form>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+        <Link to="/register">Create an account</Link>
+      </Form>
+    </>
+  );
 }
+
+/*
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">{loading ? 'Loading...' : 'Login'}</button>
+        <Link to="/register">Create an account</Link>
+      </Form>
+*/
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/logo.png';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+
+      <Form>
+        <Input name="name" type="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+        <button type="submit">Create Account</button>
+        <Link to="/">Already have an account</Link>
+      </Form>
+    </>
+  );
 }
