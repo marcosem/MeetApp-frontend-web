5afacd23d00318106a966802bedb472ead9ac8e7
Marcos Mathias <marcos-github@memathias.com>
2019-07-31 16:28:39 -0300

    Configure Routes

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '~/pages/SignIn';
+import SingUp from '~/pages/SignUp';
+
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
+import Details from '~/pages/Details';
+import MeetUp from '~/pages/MeetUp';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SingUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/details" component={Details} />
+      <Route path="/meetup" component={MeetUp} />
+    </Switch>
+  );
+}
