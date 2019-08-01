f3246332e6d11e5d9ecc4dff44781c0896cf0679
Marcos Mathias <marcos-github@memathias.com>
2019-08-01 20:33:56 -0300

    Created Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile, LogoutButton } from './styles';
+import logoHeader from '~/assets/logo-header.png';
+
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <Link to="/dashboard">
+            <img src={logoHeader} alt="MeetApp" />
+          </Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <Link to="/profile">Profile</Link>
+            </div>
+          </Profile>
+          <LogoutButton type="button">Logout</LogoutButton>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  background: #000;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 92px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    align-items: center;
+  }
+
+  aside {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+// border-left: 1px solid #eee;
+export const Profile = styled.div`
+  display: flex;
+  margin-right: 15px;
+  padding-right: 15px;
+
+  div {
+    text-align: right;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
+
+export const LogoutButton = styled.button`
+  width: 71px;
+  height: 44px;
+  background: #d44056;
+  font-weight: bold;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  transition: background 0.2s;
+
+  &:hover {
+    background: ${darken(0.08, '#d44056')};
+  }
+`;
